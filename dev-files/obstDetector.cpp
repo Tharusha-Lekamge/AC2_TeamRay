7c5efbe7f61d829cb30455c0d112e683a999a40b
Tharusha Lekamge <tharushalekamge.19@cse.mrt.ac.lk>
2022-05-04 14:39:53 +0530

    no worries updates

--- dev-files/obstDetector.cpp
+++ dev-files/obstDetector.cpp
@@ -18,3 +18,24 @@ public:
     ObstDetector(int servoPin, int sharpPin);
     void init(); // initialize two arrays
 };
+
+/*  hard code an array of tuples (angle,dist)
+    from the current position to a specific cell
+    in the map.
+
+    keep a blank array of same dimensions to mark obstacles
+
+    rotate the using the angle value in discrete intervals
+
+    measure the distance using sharp
+
+    compare the values of the measured (angle, disatnce)
+    with the hard coded array.
+
+    allow for errors
+
+    mark detected obstacles
+
+    returns an 2-d array of detected obstacles.
+    With relative position with current location as (0,0)
+*/

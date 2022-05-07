5780066914e20fc9387513e8a114ba7bc5080e1b
Tharusha Lekamge <tharushalekamge.19@cse.mrt.ac.lk>
2022-05-07 17:15:43 +0530

    added obstacle detection algorithm primary

--- dev-files/src/obstDetector.cpp
+++ dev-files/src/obstDetector.cpp
@@ -1,48 +1,28 @@
 #include "Servo.h"
-#include "ZSharpIR.h"
 
 class ObstDetector
 {
 private:
     Servo servo; // Servo motor
-    ZSharpIR sharp(10, 1080);
-
     int angle;
     float distance;
     int sharpModel = 1080
 
-        const int proximArray[13][13]; // Stores angles and dist to the cells
+        const int proximArray[6][11];  // Stores angles and dist to the cells
     int proximObsArray[13][13];        // Stores detected obstacles in locations
     int angles[100];
 
 public:
-    ObstDetector(int servoPin, int sharpPin);
+    ObstDetector(Servo servo, int sharpPin)
+    {
+        int analogPin = sharpPin;
+        float sensorVal = 0;
+        float sensorVolt = 0;
+        float Vr = 5.0;
+        float sum = 0;
+        float k1 = 16.7647563;
+        float k2 = -0.85803107;
+        float distance = 0;
+    };
     void init(); // initialize two arrays
 };
-
-/*  hard code an array of tuples (angle,dist)
-
-    from the current position to a specific cell
-    in the map.
-
-    keep a blank array of same dimensions to mark obstacles
-
-    rotate the using the angle value in discrete intervals
-
-    measure the distance using sharp
-
-    compare the values of the measured (angle, disatnce)
-    with the hard coded array.
-
-    allow for errors
-
-    mark detected obstacles
-
-    returns an 2-d array of detected obstacles.
-    With relative position with current location as (0,0)
-*/
-
-ObstDetector::ObstDetector(int servoPin, int sharpPin)
-{
-    servo.attach(servoPin);
-}
